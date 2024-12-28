4e53f1b02df98c2f423f9ad68fdaf2a39e86d2ea
Gonzalo <gonzalorivero97@gmail.com>
2024-12-28 13:15:39 -0300

    Primera subida del proyecto

---
+++ pryLetraEnPalabra/frmLetra.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace pryLetraEnPalabra
+{
+    partial class frmLetra
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frase:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Letra:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCantidad.Location = new System.Drawing.Point(206, 231);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(152, 32);
+            this.lblCantidad.TabIndex = 2;
+            this.lblCantidad.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 238);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cantidad:";
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(62, 312);
+            this.btnContar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(252, 56);
+            this.btnContar.TabIndex = 4;
+            this.btnContar.Text = "Contar con while";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(97, 60);
+            this.txtFrase.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtFrase.MaxLength = 30;
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(261, 27);
+            this.txtFrase.TabIndex = 5;
+            this.txtFrase.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.Location = new System.Drawing.Point(285, 143);
+            this.txtLetra.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtLetra.MaxLength = 1;
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(73, 27);
+            this.txtLetra.TabIndex = 6;
+            this.txtLetra.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // frmLetra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(402, 403);
+            this.Controls.Add(this.txtLetra);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frmLetra";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador de letras en frases";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.TextBox txtLetra;
+    }
+}
+
---
+++ pryLetraEnPalabra/frmLetra.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryLetraEnPalabra
+{
+    public partial class frmLetra : Form
+    {
+        public frmLetra()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            Char[]  Frase = txtFrase.Text.ToCharArray(); //Convierte el valor de la caja de texto en un vector de tipo char
+            Char Letra = Convert.ToChar(txtLetra.Text); //Convierte el valor de la caja de texto en una variable de tipo char
+            Int32 Indice = 0;
+            Int32 Cantidad = 0;
+
+            while (Indice < Frase.Length) //"Frase.lenght" sirve para contar la cantidad de caracteres que tiene el vector
+            {
+                if (Frase[Indice] == Letra) //si el valor en la posisicion del indice es igual hacer...
+                {
+                    Cantidad++;     // incrementa la cantidad en 1                
+                }
+                Indice++; //incrementa la posicion del indice en 1
+            }
+            lblCantidad.Text = Cantidad.ToString();
+        }
+    }
+}
